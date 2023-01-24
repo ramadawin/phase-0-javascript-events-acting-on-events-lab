09f15872a7f6604e8856e30d68ff49bc7851c1aa
Ramada Nguyen <ramadawin212@gmail.com>
2023-01-23 20:21:36 -0600

    commit tree 7325609115e15cd41c630ef6db8ffccebb59c430

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "100px";
+dodger.style.bottom = "0px";
+    document.addEventListener("keydown", function (event) {
+    console.log(event);
+    });         
+      function moveDodgerLeft() {
+        const leftNumbers = dodger.style.left.replace("px", "");
+        const left = parseInt(leftNumbers, 10);
+      
+        if (left > 0) {
+          dodger.style.left = `${left - 1}px`;
+        }
+      }
+      document.addEventListener("keydown", function (e) {
+        if (e.key === "ArrowLeft") {
+          moveDodgerLeft();
+        }
+      }); 
+      document.addEventListener("keydown", function (event) {
+      if (event.key === "ArrowRight"){
+        moveDodgerRight();
+      }
+      });
+    function moveDodgerRight(){
+      const leftNumbers=dodger.style.left.replace("px","");
+      const left = parseInt(leftNumbers, 10);
+      if (left < 360){
+        dodger.style.left = `${left + 1}px` 
+      }
+    }
